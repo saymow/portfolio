3df833ceb2d6ac502baf800046a9f2cedb6947f8
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-02-27 17:52:54 -0300

    feat: add scroll to top button

--- src/app/components/Presentation/index.tsx
+++ src/app/components/Presentation/index.tsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import styles from "./presentation.module.css";
 import { motion } from "framer-motion";
 import CtaButton from "../CtaButton";
 import Backdrop from "./Backdrop";
 import { ChevronDoubleDown } from "@styled-icons/heroicons-outline/ChevronDoubleDown";
+
+interface Ref {
+  x: number;
+  y: number;
+}
+
+enum ScrollTo {
+  Top,
+  Bottom,
+}
 
 const phrases = [
   "to solve complex problems with code.",
@@ -11,26 +21,57 @@ const phrases = [
   "to face new challenges.",
 ];
 
+const computeRef = (ref: Ref) => {
+  const { x, y } = ref;
+
+  if (x === phrases[y].length) {
+    return { x: 0, y: (y + 1) % phrases.length };
+  }
+
+  return { x: x + 1, y };
+};
+
 const Presentation: React.FC = () => {
-  const [refs, setReferences] = useState({ y: 0, x: 0 });
+  const [refs, setReferences] = useState<Ref>({ y: 0, x: 0 });
+  const [scrollTo, setScrollTo] = useState<ScrollTo>(ScrollTo.Bottom);
+
+  const handleScrollEvent = useCallback((_: Event) => {
+    setScrollTo(window.scrollY === 0 ? ScrollTo.Bottom : ScrollTo.Top);
+  }, []);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setReferences((prev_ref) => {
-        if (prev_ref.x === phrases[prev_ref.y].length) {
-          const new_y = (prev_ref.y + 1) % phrases.length;
-
-          return { x: 0, y: new_y };
-        }
-
-        return { x: prev_ref.x + 1, y: prev_ref.y };
-      });
-    }, 100);
+    const intervalId = setInterval(() => setReferences(computeRef), 100);
 
     return () => {
       clearInterval(intervalId);
     };
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollEvent);
+    };
+  }, []);
+
+  const handleScrollClickEvent = (e: MouseEvent) => {
+    if (scrollTo === ScrollTo.Bottom) {
+      setScrollTo(ScrollTo.Top);
+      return;
+    }
+
+    e.preventDefault();
+    window.scrollTo(0, 0);
+  };
+
+  const description = useMemo(
+    () => `
+    I like ${phrases[refs.y].slice(0, refs.x)}
+    ${refs.x % 2 === 0 ? "_" : ""}
+    `,
+    [refs]
+  );
 
   return (
     <section className={styles.container}>
@@ -55,23 +96,24 @@ const Presentation: React.FC = () => {
           >
             Software Engineer
           </motion.h2>
-          <h4>
-            I like {phrases[refs.y].slice(0, refs.x)}
-            {refs.x % 2 === 0 ? "_" : ""}
-          </h4>
+          <h4>{description}</h4>
         </div>
         <CtaButton text={"Contact me"} href="#contacts" />
       </motion.article>
       <motion.a
         href="#projects"
-        className={styles.scroll_button}
+        onClick={handleScrollClickEvent}
+        className={`${styles.scroll_button} ${
+          scrollTo === ScrollTo.Top
+            ? styles.scroll_button_top
+            : styles.scroll_button_bottom
+        }`}
         initial={{ opacity: 0, transform: "translateY(-10%)" }}
         whileInView={{ opacity: 1, transform: "translateY(-0%)" }}
         whileHover={{ opacity: 0.6 }}
         whileTap={{ opacity: 0.2 }}
       >
-        <ChevronDoubleDown height="3rem" width="3rem" />
-        <span>Scroll</span>
+        <ChevronDoubleDown height="2rem" width="2rem" />
       </motion.a>
     </section>
   );
